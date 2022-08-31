ded3de268f8bb082cbb20e5ced026f6c4a85c6a1
Polar-jaesung <76908645+Polar-jaesung@users.noreply.github.com>
2022-08-31 20:35:07 +0700

    Jeepgo: Google map, 2icons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
  // Activate Bootstrap scrollspy on the main nav element
-   
- const navBar = document.body.querySelector('#navBar');
- if (navBar) {
-     new bootstrap.ScrollSpy(document.body, {
-         target: '#navBar'
-     });
- };
+    
+const navBar = document.body.querySelector('#navBar');
+if (navBar) {
+    new bootstrap.ScrollSpy(document.body, {
+        target: '#navBar'
+    });
+};
+
+function moveJeepgo(){
+    location.href ="https://food.grab.com/vn/en/restaurant/jeepgo-coffee/5-C2DZTXBJL7WTV6?exp_src=share&itemID=5-C2DZTXBJL7WTV6-C2DZTXCAG4MEJ6";
+}
